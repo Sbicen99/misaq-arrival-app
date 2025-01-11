40c6c4c0185585008cecda20ff6ab3fe3c76518c
Sbicen99 <sercanbicen99@gmail.com>
2025-01-11 02:38:18 +0100

    added env for config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBiFHwQ_va2VdlOmE7PCNQZ6m1Z-p92Pyk",
-  authDomain: "misaq-arrival-app.firebaseapp.com",
-  projectId: "misaq-arrival-app",
-  storageBucket: "misaq-arrival-app.firebasestorage.app",
-  messagingSenderId: "443610548319",
-  appId: "1:443610548319:web:5c40e80e0a84e6463773ea",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
