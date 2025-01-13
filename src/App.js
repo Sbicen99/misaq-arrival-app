2824a519d175e2ae23ec2c336f8ebdb61d59da0e
Sbicen99 <sercanbicen99@gmail.com>
2025-01-13 01:38:02 +0100

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use BrowserRouter (not Router)
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom"; // Use BrowserRouter (not Router)
 import Arrival from "./pages/arrivalPage";
 import AdminPanel from "./pages/adminPanel";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Arrival />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route exact path="/" component={Arrival} />
+        <Route path="/admin" component={AdminPanel} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
